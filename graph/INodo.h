64402f561bf7c217ae074fd396ce74da086eefc5
XoulEater <99497779+XoulEater@users.noreply.github.com>
2022-11-22 14:10:40 -0600

    Graph

---
+++ graph/INodo.h
@@ -0,0 +1,21 @@
+#ifndef _INODO_
+#define _INODO_ 1
+
+class INodo
+{
+public:
+    int getId()
+    {
+        return id;
+    }
+
+    void setId(int pId)
+    {
+        this->id = pId;
+    }
+
+protected:
+    int id;
+};
+
+#endif
